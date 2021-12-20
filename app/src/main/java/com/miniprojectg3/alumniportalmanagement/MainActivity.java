b3413383a5eb964ecf14f2e914b4f3acda7c3576
Nishant Shingate <nishantks121@gmail.com>
2021-12-20 23:49:49 +0530

    Created The Login Fragment Design

--- app/src/main/java/com/miniprojectg3/alumniportalmanagement/MainActivity.java
+++ app/src/main/java/com/miniprojectg3/alumniportalmanagement/MainActivity.java
@@ -1,13 +1,15 @@
 package com.miniprojectg3.alumniportalmanagement;
 
+import android.app.FragmentTransaction;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.FragmentManager;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.client.config.RequestConfig;
@@ -28,7 +30,17 @@ public class MainActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+
         setContentView(R.layout.activity_main);
+
+
+        Fragment mFragment = null;
+        mFragment = new LoginFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, mFragment).commit();
 
         // InsertData insertData = new InsertData();
         //insertData.registerCustomer("Shyam","Nopassword","CSE","emila@mail.com","Coordinator");
